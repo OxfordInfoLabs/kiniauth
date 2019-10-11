3fafdc7f3e992989a3af5e74c2d12e215d635e0f
Nathan Alan <nathan@oxil.co.uk>
2019-10-11 09:33:37 +0100

    new logic.

---
+++ src/Controllers/customer/contact.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Kiniauth\Controllers\Customer;
+
+
+class Contact {
+
+    use \Kiniauth\WebServices\ControllerTraits\Customer\Contact;
+
+}
---
+++ src/WebServices/ControllerTraits/Admin/Contact.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Kiniauth\WebServices\ControllerTraits\Admin;
+
+use Kiniauth\Objects\Account\AccountSummary;
+use Kiniauth\Objects\Security\User;
+use Kiniauth\Services\Account\AccountService;
+
+trait Contact {
+
+
+    private $contactService;
+
+    /**
+     * Account constructor.
+     * @param \Kiniauth\Services\Account\ContactService $contactService
+     */
+    public function __construct($contactService) {
+        $this->contactService = $contactService;
+    }
+
+    /**
+     * Get a contact by id, or a blank one
+     *
+     * @http GET /
+     *
+     * @param null $contactId
+     * @return \Kiniauth\Objects\Account\Contact|mixed
+     */
+    public function getContact($contactId = null) {
+        return $this->contactService->getContact($contactId);
+    }
+
+    /**
+     * Save a Contact object
+     *
+     * @http POST /save
+     *
+     * @param \Kiniauth\Objects\Account\Contact $contact
+     * @return \Kiniauth\Objects\Account\Contact
+     */
+    public function saveContact($contact) {
+        return $this->contactService->saveContact($contact);
+    }
+}
