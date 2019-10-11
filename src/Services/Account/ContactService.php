e5e9aa909dfcab910639200549d8670e4a5460a6
Nathan Alan <nathan@oxil.co.uk>
2019-10-11 15:10:04 +0100

    contact functions

--- src/Services/Account/ContactService.php
+++ src/Services/Account/ContactService.php
@@ -7,8 +7,23 @@ namespace Kiniauth\Services\Account;
 use Kiniauth\Objects\Account\Account;
 use Kiniauth\Objects\Account\AccountSummary;
 use Kiniauth\Objects\Account\Contact;
+use Kinikit\Persistence\Database\Connection\DatabaseConnection;
 
 class ContactService {
+
+    /**
+     * @var DatabaseConnection
+     */
+    private $databaseConnection;
+
+    /**
+     * ContactInterceptor constructor.
+     *
+     * @param DatabaseConnection $databaseConnection
+     */
+    public function __construct($databaseConnection) {
+        $this->databaseConnection = $databaseConnection;
+    }
 
     /**
      * @param null $contactId
@@ -31,9 +46,31 @@ class ContactService {
         return $contact;
     }
 
+    /**
+     * Remove a contact
+     *
+     * @param $contactId
+     */
     public function deleteContact($contactId) {
-        Contact::fetch($contactId);
+        $contact = Contact::fetch($contactId);
+        $contact->remove();
+    }
 
+    /**
+     * Set the default contact for this account
+     *
+     * @param $contactId
+     * @param string $accountId
+     * @return Contact
+     * @throws \Kinikit\Persistence\Database\Exception\SQLException
+     */
+    public function setDefaultContact($contactId, $accountId = Account::LOGGED_IN_ACCOUNT) {
+        $this->databaseConnection->execute("UPDATE ka_contact SET default_contact = 0 WHERE account_id = ? AND default_contact = 1", $accountId);
+        /** @var Contact $contact */
+        $contact = Contact::fetch($contactId);
+        $contact->setDefaultContact(1);
+        $contact->save();
+        return $contact;
     }
 
     /**
