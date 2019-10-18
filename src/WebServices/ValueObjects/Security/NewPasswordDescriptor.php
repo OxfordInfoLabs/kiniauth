c34ac927475393d1f788c623271a7750d7da7140
Mark Robertshaw <mark@oxil.co.uk>
2019-10-18 16:57:36 +0100

    New Kiniauth logic.

---
+++ src/WebServices/ValueObjects/Security/NewPasswordDescriptor.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Kiniauth\WebServices\ValueObjects\Security;
+
+
+/**
+ * Payload object for resetting passwords
+ *
+ * Class NewPasswordDescriptor
+ */
+class NewPasswordDescriptor {
+
+    /**
+     * New password
+     *
+     * @var string
+     */
+    private $newPassword;
+
+    /**
+     * 16 digit reset code
+     *
+     * @var string
+     */
+    private $resetCode;
+
+    /**
+     * @return string
+     */
+    public function getNewPassword() {
+        return $this->newPassword;
+    }
+
+    /**
+     * @param string $newPassword
+     */
+    public function setNewPassword($newPassword) {
+        $this->newPassword = $newPassword;
+    }
+
+    /**
+     * @return string
+     */
+    public function getResetCode() {
+        return $this->resetCode;
+    }
+
+    /**
+     * @param string $resetCode
+     */
+    public function setResetCode($resetCode) {
+        $this->resetCode = $resetCode;
+    }
+
+
+}
