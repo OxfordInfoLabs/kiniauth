11df0edbdbd39f087c9535661894c9c5d351f7ab
Nathan Alan <nathan@oxil.co.uk>
2020-10-14 13:31:47 +0100

    updated account users

--- angular/projects/ng-kiniauth/src/lib/views/account-users/account-users.component.ts
+++ angular/projects/ng-kiniauth/src/lib/views/account-users/account-users.component.ts
@@ -15,6 +15,7 @@ import { Router } from '@angular/router';
 export class AccountUsersComponent implements OnInit {
 
     @Input() userRoleRoute: string;
+    @Input() disableInvite: boolean;
 
     public users: any[];
     public searchText = new BehaviorSubject<string>('');
