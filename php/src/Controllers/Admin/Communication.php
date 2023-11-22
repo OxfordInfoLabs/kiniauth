2fb3fb93d8eecc83165b47f15953a81df3a95cf4
mark@oxil.co.uk <mark@oxil.co.uk>
2023-11-22 08:37:47 +0000

    New trait

--- php/src/Controllers/Admin/Communication.php
+++ php/src/Controllers/Admin/Communication.php
@@ -2,45 +2,8 @@
 
 namespace Kiniauth\Controllers\Admin;
 
-use Kiniauth\Services\Communication\Email\EmailService;
-use Kiniauth\ValueObjects\Communication\StoredEmailSummaryItem;
+
 
 class Communication {
-
-    /**
-     * @var EmailService
-     */
-    private $emailService;
-
-
-    /**
-     * @param EmailService $emailService
-     */
-    public function __construct($emailService) {
-        $this->emailService = $emailService;
-    }
-
-    /**
-     * @http POST /email/filter
-     *
-     * @param $filters
-     * @return StoredEmailSummaryItem[]
-     */
-    public function filterStoredEmails($filters, $limit = 10, $offset = 0) {
-        return array_map(function ($storedEmailSummary) {
-            return StoredEmailSummaryItem::fromStoredEmailSummary($storedEmailSummary);
-        }, $this->emailService->filterStoredEmails($filters, $limit, $offset));
-    }
-
-
-    /**
-     * @http GET /email/$id
-     *
-     * @param integer $id
-     * @return string
-     */
-    public function getStoredEmailContents($id) {
-        return $this->emailService->getStoredEmail($id)->getTextBody();
-    }
-
+    use \Kiniauth\Traits\Controller\Admin\Communication;
 }
---
+++ php/src/Traits/Controller/Admin/Communication.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Kiniauth\Traits\Controller\Admin;
+
+use Kiniauth\Services\Communication\Email\EmailService;
+use Kiniauth\ValueObjects\Communication\StoredEmailSummaryItem;
+
+trait Communication {
+
+    /**
+     * @var EmailService
+     */
+    private $emailService;
+
+
+    /**
+     * @param EmailService $emailService
+     */
+    public function __construct($emailService) {
+        $this->emailService = $emailService;
+    }
+
+    /**
+     * @http POST /email/filter
+     *
+     * @param $filters
+     * @return StoredEmailSummaryItem[]
+     */
+    public function filterStoredEmails($filters, $limit = 10, $offset = 0) {
+        return array_map(function ($storedEmailSummary) {
+            return StoredEmailSummaryItem::fromStoredEmailSummary($storedEmailSummary);
+        }, $this->emailService->filterStoredEmails($filters, $limit, $offset));
+    }
+
+
+    /**
+     * @http GET /email/$id
+     *
+     * @param integer $id
+     * @return string
+     */
+    public function getStoredEmailContents($id) {
+        return $this->emailService->getStoredEmail($id)->getTextBody();
+    }
+
+}
