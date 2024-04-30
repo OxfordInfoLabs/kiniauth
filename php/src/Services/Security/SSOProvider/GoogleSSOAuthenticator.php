ece5905772d7559eaf3ae4c309afbca79d87d669
PeterSpainOXIL <114655461+PeterSpainOXIL@users.noreply.github.com>
2024-04-30 11:35:29 +0100

    google sso initial

--- php/src/Services/Security/SSOProvider/GoogleSSOAuthenticator.php
+++ php/src/Services/Security/SSOProvider/GoogleSSOAuthenticator.php
@@ -2,6 +2,25 @@
 
 namespace Kiniauth\Services\Security\SSOProvider;
 
-class GoogleSSOAuthenticator {
+use Kinikit\Core\Configuration\Configuration;
+
+class GoogleSSOAuthenticator extends SSOAuthenticator {
+
+    public function authenticate($data) {
+
+        $clientId =  Configuration::readParameter("sso.google.clientId");
+        $client = new \Google_Client(["client_id" => $clientId]);
+
+        $payload = $client->verifyIdToken($data);
+        if ($payload) {
+            $userId = $payload["sub"];
+            $email = $payload["email"] ?? null;
+        } else {
+            throw new \Exception("Invalid ID token");
+        }
+
+        return $email;
+
+    }
 
 }
