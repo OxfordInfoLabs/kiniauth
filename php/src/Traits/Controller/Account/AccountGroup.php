f3f0d5f582442c8af1da5f318686814189d5830d
PeterSpainOXIL <114655461+PeterSpainOXIL@users.noreply.github.com>
2025-10-02 15:53:21 +0100

    endpoints for account get and accept group invitations

--- php/src/Traits/Controller/Account/AccountGroup.php
+++ php/src/Traits/Controller/Account/AccountGroup.php
@@ -103,7 +103,31 @@ trait AccountGroup {
         return $this->accountGroupService->getActiveAccountGroupInvitationAccounts($accountGroupId);
     }
 
+    /**
+     * Get the details for an invitation using an invitation code.
+     *
+     * @http GET /invitation/$invitationCode
+     *
+     * @param string $invitationCode
+     * 
+     * @return AccountGroupInvitation
+     */
+    public function getInvitationDetails($invitationCode) {
+        return $this->accountGroupService->getInvitationDetails($invitationCode);
+    }
 
+    /**
+     * Accept an invitation to join an account group
+     *
+     * @http POST /invitation/$invitationCode
+     *
+     * @param string $invitationCode
+     *
+     * @return void
+     */
+    public function acceptInviteToAccountGroup($invitationCode) {
+        $this->accountGroupService->acceptAccountGroupInvitation($invitationCode);
+    }
 
     /**
      * Revoke an invitation to an account group
