e0ae8a051a58461977b75833bc9764c2b4de4fed
PeterSpainOXIL <114655461+PeterSpainOXIL@users.noreply.github.com>
2026-04-08 09:05:59 +0100

    add override for SAML base URL

--- php/src/Services/Security/SSOProvider/SAMLAuthenticator.php
+++ php/src/Services/Security/SSOProvider/SAMLAuthenticator.php
@@ -7,6 +7,7 @@ use Kinikit\Core\Logging\Logger;
 use OneLogin\Saml2\Auth;
 use OneLogin\Saml2\Response;
 use OneLogin\Saml2\Settings;
+use OneLogin\Saml2\Utils;
 
 class SAMLAuthenticator {
 
@@ -30,6 +31,14 @@ class SAMLAuthenticator {
 
     public function authenticate(mixed $data) {
         $response = new Response($this->settings, $data["SAMLResponse"]);
+
+        // Set the incoming base URL to allow for proxying
+        $document = $response->document;
+        if ($document->documentElement->hasAttribute('Destination')) {
+            $destination = $document->documentElement->getAttribute('Destination');
+            Utils::setBaseURL($destination);
+        }
+
         if ($response->isValid()) {
             return $response->getAttributes()["email"][0] ?? null;
         } else {
