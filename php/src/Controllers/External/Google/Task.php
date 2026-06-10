b0caff03f3c60b43059f966dc89278d404d0836e
matthewcamusOXIL <matthew.camus@oxil.co.uk>
2026-06-10 14:22:12 +0100

    Moved queued task controller from GSE to kiniauth

---
+++ php/src/Controllers/External/Google/Task.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Kiniauth\Controllers\External\Google;
+
+use Kiniauth\Exception\QueuedTask\NoQueuedTaskImplementationException;
+use Kinikit\Core\Configuration\ConfigFile;
+use Kinikit\Core\Configuration\FileResolver;
+use Kinikit\Core\DependencyInjection\Container;
+use Kinikit\MVC\Request\Request;
+use Kinikit\MVC\Response\JSONResponse;
+use Kinikit\MVC\Response\Response;
+
+class Task {
+
+    /**
+     * @var FileResolver
+     */
+    private $fileResolver;
+
+    /**
+     * @var string[string]
+     */
+    private $taskClasses;
+
+    /**
+     * Google Queued Task Controller constructor.
+     *
+     * @param FileResolver $fileResolver
+     */
+    public function __construct($fileResolver) {
+        $this->fileResolver = $fileResolver;
+    }
+
+    /**
+     * @http POST /
+     *
+     * @param array $payload
+     * @return void
+     * @throws NoQueuedTaskImplementationException
+     */
+    public function processTask($payload): void {
+        $this->loadTaskClasses();
+
+        $taskIdentifier = $payload["taskIdentifier"];
+        $taskConfiguration = $payload["configuration"];
+
+        if (isset($this->taskClasses[$taskIdentifier])) {
+            Container::instance()->get($this->taskClasses[$taskIdentifier])->run($taskConfiguration);
+        }
+        else {
+            throw new NoQueuedTaskImplementationException($taskIdentifier);
+        }
+    }
+
+    private function loadTaskClasses() {
+        if (!$this->taskClasses) {
+            $this->taskClasses = [];
+            foreach ($this->fileResolver->getSearchPaths() as $searchPath) {
+                $config = new ConfigFile($searchPath . "/Config/queued-tasks.txt");
+                $this->taskClasses = array_merge($this->taskClasses, $config->getAllParameters());
+            }
+        }
+    }
+}
